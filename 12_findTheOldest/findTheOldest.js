5a1fbc18ff5740e466a7279a74625b33846cdc31
Judah <judahadefuye1@outlook.com>
2024-02-13 00:04:11 +0000

    Finish

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
 const findTheOldest = function(people) {
-    return people.reduce((current, person) => { if person.yearOfDeath})
+    "yearOfDeath" in people[0]? people[0] : people[0]["yearOfDeath"] = new Date().getFullYear();
+    return people.reduce((a, b) => { 
+        if ("yearOfDeath" in b){
+            return (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) ? a :b;
+        }else{
+            let today = new Date().getFullYear();
+            return (a.yearOfDeath - a.yearOfBirth > today - b.yearOfBirth) ? a :b;
+        } },  )
 };
 
 // Do not edit below this line
